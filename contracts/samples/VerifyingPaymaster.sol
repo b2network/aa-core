a6055c2f41d38356d091a44385e5890048262178
thecookfrankie <95396011+thecookfrankie@users.noreply.github.com>
2023-11-21 14:12:26 +0800

    fix(VerifyingPaymaster): adjust UO packing

--- contracts/samples/VerifyingPaymaster.sol
+++ contracts/samples/VerifyingPaymaster.sol
@@ -4,8 +4,10 @@ pragma solidity ^0.8.12;
 /* solhint-disable reason-string */
 /* solhint-disable no-inline-assembly */
 
-import "../core/BasePaymaster.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "../core/BasePaymaster.sol";
+import { calldataKeccak } from "../core/Helpers.sol";
+
 /**
  * A sample paymaster that uses external service to decide whether to pay for the UserOp.
  * The paymaster trusts an external signer to sign the transaction.
@@ -16,7 +18,6 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
  * - the account checks a signature to prove identity and account ownership.
  */
 contract VerifyingPaymaster is BasePaymaster {
-
     using ECDSA for bytes32;
     using UserOperationLib for UserOperation;
 
@@ -26,26 +27,33 @@ contract VerifyingPaymaster is BasePaymaster {
 
     uint256 private constant SIGNATURE_OFFSET = 84;
 
-    constructor(IEntryPoint _entryPoint, address _verifyingSigner) BasePaymaster(_entryPoint) {
+    constructor(
+        IEntryPoint _entryPoint,
+        address _verifyingSigner
+    ) BasePaymaster(_entryPoint) {
         verifyingSigner = _verifyingSigner;
     }
 
     mapping(address => uint256) public senderNonce;
 
-    function pack(UserOperation calldata userOp) internal pure returns (bytes memory ret) {
-        // lighter signature scheme. must match UserOp.ts#packUserOp
-        bytes calldata pnd = userOp.paymasterAndData;
-        // copy directly the userOp from calldata up to (but not including) the paymasterAndData.
-        // this encoding depends on the ABI encoding of calldata, but is much lighter to copy
-        // than referencing each field separately.
-        assembly {
-            let ofs := userOp
-            let len := sub(sub(pnd.offset, ofs), 32)
-            ret := mload(0x40)
-            mstore(0x40, add(ret, add(len, 32)))
-            mstore(ret, len)
-            calldatacopy(add(ret, 32), ofs, len)
-        }
+    function pack(
+        UserOperation calldata userOp
+    ) internal pure returns (bytes memory ret) {
+        bytes32 hashInitCode = calldataKeccak(userOp.initCode);
+        bytes32 hashCallData = calldataKeccak(userOp.callData);
+
+        return
+            abi.encode(
+                userOp.sender,
+                userOp.nonce,
+                hashInitCode,
+                hashCallData,
+                // userOp.callGasLimit,
+                // userOp.verificationGasLimit
+                // userOp.preVerificationGas,
+                userOp.maxFeePerGas,
+                userOp.maxPriorityFeePerGas
+            );
     }
 
     /**
@@ -55,18 +63,23 @@ contract VerifyingPaymaster is BasePaymaster {
      * note that this signature covers all fields of the UserOperation, except the "paymasterAndData",
      * which will carry the signature itself.
      */
-    function getHash(UserOperation calldata userOp, uint48 validUntil, uint48 validAfter)
-    public view returns (bytes32) {
+    function getHash(
+        UserOperation calldata userOp,
+        uint48 validUntil,
+        uint48 validAfter
+    ) public view returns (bytes32) {
         //can't use userOp.hash(), since it contains also the paymasterAndData itself.
-
-        return keccak256(abi.encode(
-                pack(userOp),
-                block.chainid,
-                address(this),
-                senderNonce[userOp.getSender()],
-                validUntil,
-                validAfter
-            ));
+        return
+            keccak256(
+                abi.encode(
+                    pack(userOp),
+                    block.chainid,
+                    address(this),
+                    senderNonce[userOp.getSender()],
+                    validUntil,
+                    validAfter
+                )
+            );
     }
 
     /**
@@ -76,29 +89,48 @@ contract VerifyingPaymaster is BasePaymaster {
      * paymasterAndData[20:84] : abi.encode(validUntil, validAfter)
      * paymasterAndData[84:] : signature
      */
-    function _validatePaymasterUserOp(UserOperation calldata userOp, bytes32 /*userOpHash*/, uint256 requiredPreFund)
-    internal override returns (bytes memory context, uint256 validationData) {
+    function _validatePaymasterUserOp(
+        UserOperation calldata userOp,
+        bytes32 /*userOpHash*/,
+        uint256 requiredPreFund
+    ) internal override returns (bytes memory context, uint256 validationData) {
         (requiredPreFund);
 
-        (uint48 validUntil, uint48 validAfter, bytes calldata signature) = parsePaymasterAndData(userOp.paymasterAndData);
-        //ECDSA library supports both 64 and 65-byte long signatures.
+        (
+            uint48 validUntil,
+            uint48 validAfter,
+            bytes calldata signature
+        ) = parsePaymasterAndData(userOp.paymasterAndData);
+        // ECDSA library supports both 64 and 65-byte long signatures.
         // we only "require" it here so that the revert reason on invalid signature will be of "VerifyingPaymaster", and not "ECDSA"
-        require(signature.length == 64 || signature.length == 65, "VerifyingPaymaster: invalid signature length in paymasterAndData");
-        bytes32 hash = ECDSA.toEthSignedMessageHash(getHash(userOp, validUntil, validAfter));
+        require(
+            signature.length == 64 || signature.length == 65,
+            "VerifyingPaymaster: invalid signature length in paymasterAndData"
+        );
+        bytes32 hash = getHash(userOp, validUntil, validAfter);
         senderNonce[userOp.getSender()]++;
 
-        //don't revert on signature failure: return SIG_VALIDATION_FAILED
-        if (verifyingSigner != ECDSA.recover(hash, signature)) {
-            return ("",_packValidationData(true,validUntil,validAfter));
+        // don't revert on signature failure: return SIG_VALIDATION_FAILED
+        if (verifyingSigner != ECDSA.recover(ECDSA.toEthSignedMessageHash(hash), signature)) {
+            return ("", _packValidationData(true, validUntil, validAfter));
         }
 
-        //no need for other on-chain validation: entire UserOp should have been checked
+        // no need for other on-chain validation: entire UserOp should have been checked
         // by the external service prior to signing it.
-        return ("",_packValidationData(false,validUntil,validAfter));
+        return ("", _packValidationData(false, validUntil, validAfter));
     }
 
-    function parsePaymasterAndData(bytes calldata paymasterAndData) public pure returns(uint48 validUntil, uint48 validAfter, bytes calldata signature) {
-        (validUntil, validAfter) = abi.decode(paymasterAndData[VALID_TIMESTAMP_OFFSET:SIGNATURE_OFFSET],(uint48, uint48));
+    function parsePaymasterAndData(
+        bytes calldata paymasterAndData
+    )
+        public
+        pure
+        returns (uint48 validUntil, uint48 validAfter, bytes calldata signature)
+    {
+        (validUntil, validAfter) = abi.decode(
+            paymasterAndData[VALID_TIMESTAMP_OFFSET:SIGNATURE_OFFSET],
+            (uint48, uint48)
+        );
         signature = paymasterAndData[SIGNATURE_OFFSET:];
     }
 }
